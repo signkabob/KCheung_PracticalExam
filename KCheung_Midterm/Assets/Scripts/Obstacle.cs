1be1b640211ad2f552d72f2661da5ef1c91e781e
Kabo <signkabob@gmail.com>
2026-07-05 15:01:25 -0500

    End of Class Commit
    - Implemented Obstacle partially

--- KCheung_Midterm/Assets/Scripts/Obstacle.cs
+++ KCheung_Midterm/Assets/Scripts/Obstacle.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [SerializeField] private float minSpeed = 1;
+    [SerializeField] private float maxSpeed = 5;
+    [SerializeField] private int minDamage = 1;
+    [SerializeField] private int maxDamage = 5;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
