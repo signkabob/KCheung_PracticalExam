1cb63e026e881f04f51687849833064ffe2be9c5
Kabo <signkabob@gmail.com>
2026-07-05 13:13:24 -0500

    Implemented Coin Movement

--- KCheung_Midterm/Assets/Scripts/Coin.cs
+++ KCheung_Midterm/Assets/Scripts/Coin.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     [SerializeField] private int score;
-    [SerializeField] private float rotateSpeed = 30;
-
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float rotateSpeed = 60;
+    [SerializeField] private float minY = 0.5f;
+    [SerializeField] private float maxY = 2.5f;
+    [SerializeField] private bool moveUp = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +18,28 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(transform.rotation.x, rotateSpeed * Time.deltaTime, transform.rotation.z);
+
+        // Should be rotating around y-axis
+        transform.rotation = Quaternion.Euler(transform.rotation.x, rotateSpeed * Time.deltaTime, transform.rotation.y);
+        
+        // Moving up and down 
+        if (moveUp)
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+            if (transform.position.y >= maxY)
+            {
+                moveUp = false;
+            }
+        }
+        else
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+            if (transform.position.y <= minY)
+            {
+                moveUp = true;
+            }
+        }
+        
+        
     }
 }
